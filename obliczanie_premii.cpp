cafc44afd7e9455afa4c8238c3b9bd7d49b7b4f9
LOL-li-POP <90280162+LOL-li-POP@users.noreply.github.com>
2023-11-29 08:53:24 +0100

    obliczanie_premii.cpp

--- obliczanie_premii.cpp
+++ obliczanie_premii.cpp
@@ -1,16 +1,25 @@
 float obliczaniePremii(float wartoscSprzedazy) {
     float wysokoscPremii;
 
-    if (wartoscSprzedazy < 10000) {
-        wysokoscPremii = 0.10; // 10%
-    } else if (wartoscSprzedazy >= 10000 && wartoscSprzedazy <= 14999) {
-        wysokoscPremii = 0.12; // 12%
-    } else if (wartoscSprzedazy >= 15000 && wartoscSprzedazy <= 17999) {
-        wysokoscPremii = 0.14; // 14%
-    } else if (wartoscSprzedazy >= 18000 && wartoscSprzedazy <= 21999) {
-        wysokoscPremii = 0.16; // 16%
-    } else {
-        wysokoscPremii = 0.18; // 18%
+    if (wartoscSprzedazy < 10000)
+    {
+        wysokoscPremii = 0.10;
+    }
+    else if (wartoscSprzedazy >= 10000 && wartoscSprzedazy <= 14999)
+    {
+        wysokoscPremii = 0.12;
+    }
+    else if (wartoscSprzedazy >= 15000 && wartoscSprzedazy <= 17999)
+    {
+        wysokoscPremii = 0.14;
+    }
+    else if (wartoscSprzedazy >= 18000 && wartoscSprzedazy <= 21999)
+    {
+        wysokoscPremii = 0.16;
+    }
+    else
+    {
+        wysokoscPremii = 0.18;
     }
 
     return wysokoscPremii;
