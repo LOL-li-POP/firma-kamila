ab66bc4309595cd66a4cc2a9c8dce87cf3ae389e
LOL-li-POP <90280162+LOL-li-POP@users.noreply.github.com>
2023-11-29 08:54:12 +0100

    obliczanie_wynagrodzenia.cpp

--- obliczanie_wynagrodzenia.cpp
+++ obliczanie_wynagrodzenia.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-float obliczanieWynagrodzenia(float wartoscSprzedazy, float wartoscZaliczki) {
+float obliczanieWynagrodzenia(float wartoscSprzedazy, float wartoscZaliczki)
+{
     float wysokoscPremii = obliczaniePremii(wartoscSprzedazy);
     float wynagrodzenie = wartoscSprzedazy * wysokoscPremii - wartoscZaliczki;
-
-    if (wynagrodzenie < 0) {
+    if (wynagrodzenie < 0)
+    {
         std::cout << "Pracownik musi zwrócić pieniądze firmie." << std::endl;
     }
 
